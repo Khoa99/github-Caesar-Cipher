d5a28d712fd8a4fc6d1f09170e07d5d371ce2789
Khoa99 <74178495+Khoa99@users.noreply.github.com>
2021-05-14 12:37:27 +0900

    Update Cipher.js

--- Cipher.js
+++ Cipher.js
@@ -1,17 +1,21 @@
+/*
+  Đây là code phần mã Caesar/Cipher 
+  Language:Javascript
+*/
 function caaesar_cipher(string,mode,key){
     for(key;key<26;key++){
     var text="",i;
     var num;
-    if(mode=='ENCRYPTION'){
+    if(mode=='ENCRYPTION'){//Đây là phần mã hóa-Encryption
         for (i of string){
-            num=((i.charCodeAt()-65)+key)%26;
+            num=((i.charCodeAt()-65)+key)%26;//đưa nó về phần số xong -65 VD A là 65 , 65-65=0 đưa về vị trí trong bảng 26 Alphabet
             text+=(String.fromCharCode(num+65));
         }
         console.log("key["+key+"] "+"ENCRYPTION : "+text);
     }
-if(mode=='DECRYPTION'){
+if(mode=='DECRYPTION'){//Đây là phần giải mã -Decryption
         for (i of string){
-            num=((i.charCodeAt()+65)-key)%26;
+            num=((i.charCodeAt()+65)-key)%26;// phần này sẽ đưa về vị trí  26 bảng Alphabet
             text+=(String.fromCharCode(num+65));
         }
         console.log("key["+key+"] "+"DYCRYPTION : "+text);
