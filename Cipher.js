69fec3e25ac55c02f9521aec937ee3bfb1c7b4eb
Khoa99 <74178495+Khoa99@users.noreply.github.com>
2021-05-19 14:08:30 +0900

    Update Cipher.js

--- Cipher.js
+++ Cipher.js
@@ -8,15 +8,25 @@ function caesar_cipher(string,mode,key){
     var num;
     if(mode=='ENCRYPTION'){//Đây là phần mã hóa-Encryption
         for (i of string){
+          if(i==" "){
+            text+=" ";
+          }
+          else{
             num=((i.charCodeAt()-65)+key)%26;//đưa nó về phần số xong -65 VD A là 65 , 65-65=0 đưa về vị trí trong bảng 26 Alphabet
             text+=(String.fromCharCode(num+65));
+          }
         }
         console.log("key["+key+"] "+"ENCRYPTION : "+text);
     }
 if(mode=='DECRYPTION'){//Đây là phần giải mã -Decryption
         for (i of string){
+          if(i==" "){
+            text+=" ";
+          }
+          else{
             num=((i.charCodeAt()+65)-key)%26;// phần này sẽ đưa về vị trí  26 bảng Alphabet
             text+=(String.fromCharCode(num+65));
+          }
         }
         console.log("key["+key+"] "+"DYCRYPTION : "+text);
     }
